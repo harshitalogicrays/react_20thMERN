c49a1eb79a3e4e7ca88bb90bc3562e40aad242eb
harshita <harshita@logicrays.com>
2024-09-24 16:28:07 +0530

    24sep

--- react-basics/src/classcomponents/Statedemoinclass.jsx
+++ react-basics/src/classcomponents/Statedemoinclass.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
 
-export default class s extends Component {
+export default class Statedemoinclass extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {count:'',num1:2,num2:1,result:''}
+  }
+  handleCounter=()=>{
+    this.setState({count:parseInt(this.state.count+1)})
+  }
+  handleAdd=()=>{
+    this.setState({result:this.state.num1+this.state.num2})
+  }
   render() {
     return (
-      <div>
-        sss
-      </div>
+      <>
+        <Button onClick={this.handleCounter}>Counter</Button>
+        <h1>{this.state.count}</h1>
+
+        <Button onClick={this.handleAdd}>Addition</Button>
+        <h2>{this.state.result}</h2>
+      </>
     )
   }
 }
--- react-basics/src/classcomponents/forminclass.jsx
+++ react-basics/src/classcomponents/forminclass.jsx
@@ -1,11 +1,109 @@
 import React, { Component } from 'react'
 
 export default class Forminclass extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+       user:{username:'',email:'',password:'',cpassword:''},
+       errors:{unamerr:'',emailerr:'',pwderr:'',cpwderr:''}
+    }
+    this.focusRef = React.createRef()
+  }
+  handleChange=(e)=>{
+    this.setState({
+      user:{...this.state.user, [e.target.name]:e.target.value}
+    })
+  }
+
+  checkusername=()=>{
+     if(this.state.user.username==''){
+      this.setState(prevState =>({errors:{...prevState.errors,unamerr:'username is required'}
+        }))
+        return false
+    }    
+    else{
+      this.setState(prevState => ({
+        errors:{...prevState.errors,unamerr:'',}
+         }));return true}
+}
+
+
+checkemail=()=>{
+  let pattern =  /^[\w\.]+\@[\w]+\.[a-zA-Z]{2,3}$/
+  if(this.state.user.email==''){
+    this.setState(prevState => ({
+      errors:{...prevState.errors,emailerr:'email is required',}
+       })) ;return false
+  }
+  else if (!pattern.test(this.state.user.email)){
+    this.setState(prevState => ({
+      errors:{...prevState.errors,emailerr:'invalid email',}
+       }));return false
+  }
+  else {   this.setState(prevState => ({
+    errors:{...prevState.errors,emailerr:'',}
+     }));return true}
+}
+   handleSubmit=(e)=>{
+    e.preventDefault()
+    let uname = this.checkusername()
+    let  email =this.checkemail()
+    if(uname &&  email){
+      alert(JSON.stringify(this.state.user))
+    }
+    else {e.preventDefault()}
+
+}
+componentDidMount(){ //once the comp has been loaded completely
+  this.focusRef.current.focus()
+}
   render() {
     return (
-      <div>
-        ffff
-      </div>
+      <div className=''>
+    <h1>Form Validations</h1><hr/>
+    <div className="row">
+            <form onSubmit={this.handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="" className="form-label">Username</label>
+                    <input type="text" className="form-control" name="username"  ref={this.focusRef}
+                    value={this.state.user.username}
+                    onChange={this.handleChange}
+                    onBlur={this.checkusername}
+                   />
+                   {this.state.errors.unamerr && <span className='text-danger'>{this.state.errors.unamerr}</span>}
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="" className="form-label">Email</label>
+                    <input type="text" className="form-control" name="email"
+                      value={this.state.user.email}
+                      onChange={this.handleChange} onBlur={this.checkemail}
+                   />
+                      {this.state.errors.emailerr && <span className='text-danger'>{this.state.errors.emailerr}</span>}
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="" className="form-label">Password</label>
+                    <input type="password" className="form-control" name="password"
+                      value={this.state.user.password}
+                      onChange={this.handleChange}
+                  />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="" className="form-label">Confirm Password</label>
+                    <input type="password" className="form-control" name="cpassword"
+                      value={this.state.user.cpassword}
+                      onChange={this.handleChange}
+                   />
+                    
+                </div>
+                <div className="d-grid gap-2">
+                      <button  type="submit"  className="btn btn-primary" > Submit  </button>
+                </div>
+                
+              
+                
+            </form>
+        </div>
+    </div>
     )
   }
 }
